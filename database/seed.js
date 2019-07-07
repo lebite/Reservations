3ccf068189fdf2605e7f48707f19ac5428e38f59
poema-cavalcanti <poema.campos@gmail.com>
2019-07-06 17:01:16 -0700

    major db redesign after starting on server

--- database/seed.js
+++ database/seed.js
@@ -2,39 +2,65 @@
  * Seed the database
  */
 const faker = require('faker');
+const moment = require('moment');
+
 const { Reservation } = require('./index');
+const { Restaurant } = require('./index');
 
 function seedReservations() {
-  // create some dummy reservations
+  // hardcoded open and close times to choose from
+  const openTimes = [
+    { hour: 8, minute: 30 },
+    { hour: 9, minute: 0 },
+    { hour: 9, minute: 30 },
+    { hour: 10, minute: 0 },
+    { hour: 10, minute: 30 },
+    { hour: 11, minute: 0 },
+  ];
+  const closeTimes = [
+    { hour: 18, minute: 30 },
+    { hour: 19, minute: 0 },
+    { hour: 19, minute: 30 },
+    { hour: 20, minute: 0 },
+    { hour: 20, minute: 30 },
+    { hour: 21, minute: 0 },
+  ];
+
+  // create 100 restaurants and 2 to 10 reservations per restaurant
+  let restaurants = [];
   let reservations = [];
 
   for (let i = 0; i < 100; i += 1) {
-    let max_seating = faker.random.number({ min: 15, max: 100 });
-    let max_party_size = faker.random.number({ min: 4, max: 10 });
-    let restaurant_open_time = faker.random.arrayElement([830, 900, 930, 1000, 1030, 1100]);
-    let restaurant_close_time = faker.random.arrayElement([1800, 1830, 1900, 1930, 2000]);
+    const newRestaurant = {
+      _id: i,
+      time_intervals: 15,
+      max_seating: faker.random.number({ min: 15, max: 100 }),
+      max_party_size: faker.random.number({ min: 4, max: 10 }),
+      restaurant_open_time: faker.random.arrayElement(openTimes),
+      restaurant_close_time: faker.random.arrayElement(closeTimes),
+    };
+    restaurants.push(newRestaurant);
 
     const numReservations = faker.random.number({ min: 2, max: 10 });
     for (let j = 0; j < numReservations; j += 1) {
       reservations.push({
-        bookings: [{
-          booking_time: faker.date.between('2019-07-10', '2019-07-17'),
-          party_qty: faker.random.number(max_party_size),
-        }],
-        time_intervals: 15,
-        max_seating,
-        max_party_size,
-        restaurant_open_time,
-        restaurant_close_time,
+        booking_time: faker.date.between(
+          moment(newRestaurant.restaurant_open_time),
+          moment(newRestaurant.restaurant_close_time),
+        ),
+        party_qty: faker.random.number({ min: 1, max: newRestaurant.max_party_size }),
         restaurant_id: i,
       });
     }
   }
 
-  // use the Reservation model to insert/save
+  // use mongoose models to insert/save
+  restaurants.forEach((restaurant) => {
+    new Restaurant(restaurant).save();
+  });
+
   reservations.forEach((reservation) => {
-    const newReservation = new Reservation(reservation);
-    newReservation.save();
+    new Reservation(reservation).save();
   });
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
+/* eslint-disable func-names */
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+const moment = require('moment');
+
+const { Reservation } = require('../database/index');
+const { Restaurant } = require('../database/index');
 
 const PORT = 3000;
 
@@ -12,7 +17,30 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client/')));
 
 app.get('/:restaurant_id/reservations', (req, res) => {
-  res.sendStatus(200);
+  const restaurantId = req.params.restaurant_id;
+  Restaurant.find({ _id: restaurantId }, function (err, restaurant) {
+    if (err) {
+      res.send(err);
+    } else {
+      Reservation.find({
+        restaurant_id: restaurantId,
+        booking_time: {
+          $gte: moment().valueOf(),
+          $lte: moment(restaurant.restaurant_close_time).valueOf(),
+        },
+      }, function (err, reservations) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send({
+            restaurant_id: restaurantId,
+            restaurant_information: restaurant,
+            bookings: reservations,
+          });
+        }
+      });
+    }
+  });
 });
 
 app.post('/:restaurant_id/reservations', (req, res) => {
@@ -20,5 +48,5 @@ app.post('/:restaurant_id/reservations', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}...`);
 });
