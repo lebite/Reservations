6c5ab0ccdc07c1cd4c28e3987ab86ce94811ae3d
poema-cavalcanti <poema.campos@gmail.com>
2019-07-09 17:21:35 -0700

    fix test description to be clearer

--- test/client/App.test.js
+++ test/client/App.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import App from '../../client/components/App';
 
 describe('App component', () => {
-  it('renders all three sections', () => {
+  it('renders title, form, and times sections', () => {
     const component = renderer.create(<App />);
     const json = component.toJSON();
     expect(json.children[0].children).toHaveLength(3);
