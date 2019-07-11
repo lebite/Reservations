2e2371f14391e45375f34d4b7a5030ed72c00fa5
poema-cavalcanti <poema.campos@gmail.com>
2019-07-10 19:13:11 -0700

    write tests

--- test/server/app.test.js
+++ test/server/app.test.js
@@ -4,9 +4,30 @@ const app = require('../../server/app');
 /**
  * @jest-environment node
  */
-describe('Test the root path', () => {
-  test('It should response the GET method', (done) => {
+describe('Server should respond to', () => {
+  test('GET /:restaurant_id/reservations with ', (done) => {
     request(app).get('/1/reservations').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('GET /:restaurant_id/reservations/all with ', (done) => {
+    request(app).get('/1/reservations/all').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('GET /:restaurant_id/reservations/count with ', (done) => {
+    request(app).get('/1/reservations/count').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+
+  test('POST /:restaurant_id/reservations with ', (done) => {
+    request(app).post('/1/reservations').then((response) => {
       expect(response.statusCode).toBe(200);
       done();
     });
